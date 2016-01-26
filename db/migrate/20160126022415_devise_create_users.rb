68803e5834bdbc8483c661f5fed42514d1d500ee
Ly Xuan Sang <lyxuansang1991@gmail.com>
2016-01-26 10:48:48 +0700

    Commit Signin, signout page

---
+++ db/migrate/20160126022415_devise_create_users.rb
@@ -0,0 +1,13 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false, default: ""
+      t.datetime :remember_created_at
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
